384577135b08b6abe1879fdb376b5fb462f755f4
Shivprakash chavan <142031100+shivprakash9421@users.noreply.github.com>
2026-02-12 12:27:39 +0530

    Fixed render port issue

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,11 @@ mongoose.connect(process.env.MONGO_URI)
   });
 
 // Start server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 const authRoutes = require("./routes/authRoutes");
 app.use("/api", authRoutes);
